1e81b4ed5d6de6a6e3df1edd685f7c47037b07ae
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-13 23:31:53 +0900

    Enhance PopularProductCard with hover effect and improved styling

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -2,37 +2,39 @@ import { star } from "../assets/icons";
 
 const PopularProductCard = ({ imgURL, name, price }) => {
 	return (
-		<div className="flex flex-1 flex-col w-full max-sm:w-full">
+		<div className="relative flex flex-1 flex-col w-full max-sm:w-full transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
 			{/* Product image */}
 			<img
 				src={imgURL}
 				alt={name} // Alt text for accessibility
-				className="w-[280px] h-[280px]" // Fixed width and height for the product image
+				className="w-[280px] h-[280px] object-cover rounded-t-lg" // Fixed width and height for the product image
 			/>
-			<div className="mt-8 flex justify-start gap-2.5">
-				{/* Star rating icon */}
-				<img
-					src={star}
-					alt="rating" // Alt text for accessibility
-					width={24} // Width of the star icon
-					height={24} // Height of the star icon
-				/>
+			<div className="p-4 bg-white rounded-b-lg flex flex-col h-full">
+				<div className="mt-8 flex justify-start gap-2.5">
+					{/* Star rating icon */}
+					<img
+						src={star}
+						alt="rating" // Alt text for accessibility
+						width={24} // Width of the star icon
+						height={24} // Height of the star icon
+					/>
 
-				{/* Product rating */}
-				<p className="font-montserrat text-xl leading-normal text-slate-gray">
-					(4.5)
+					{/* Product rating */}
+					<p className="font-montserrat text-xl leading-normal text-slate-gray">
+						(4.5)
+					</p>
+				</div>
+
+				{/* Display the product name */}
+				<h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
+					{name}
+				</h3>
+
+				{/* Display the product price */}
+				<p className="mt-auto font-semibold font-montserrat text-coral-red leading-normal">
+					{price}
 				</p>
 			</div>
-
-			{/* Display the product name */}
-			<h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
-				{name}
-			</h3>
-
-			{/* Display the product price */}
-			<p className="mt-2 font-semibold font-montserrat text-coral-red leading-normal">
-				{price}
-			</p>
 		</div>
 	);
 };
